1fdfaa6cdf299fe0cb7919be015e429ff2d2ac4d
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-01-05 18:09:45 +0530

    ready to deploy

---
+++ src/redux/action/Action.js
@@ -0,0 +1,38 @@
+import { ADD_GROUP, ADD_INPUT_BOX, DELETE_INPUT_BOX, SHOW_FLASH_CARDS} from '../Type'
+
+// addInputBox function will be used to add the function box.
+export const addInputBox = (data) => {
+  return {
+    type:ADD_INPUT_BOX,
+    payloade : data
+  }
+}
+
+
+//This action function will be used to delete the input box 
+export const deleteInputBox = (data) => {
+  return {
+    type:DELETE_INPUT_BOX,
+    payloade : data
+  }
+}
+
+// Add group
+export const addGroup = (data) => {
+  return {
+    type:ADD_GROUP,
+    payloade : data
+  }
+}
+
+// Show flashcards
+export const showFalshCard = (data)=>{
+  
+  return {
+    type:SHOW_FLASH_CARDS,
+    payloade:data
+  }
+}
+
+
+
